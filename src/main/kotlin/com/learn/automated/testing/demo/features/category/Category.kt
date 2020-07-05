d2d44d1f92d7acf72397bd64189555e2cc6b9bc7
Ricky <hanusacloud@gmail.com>
2020-07-05 12:04:50 +0700

    Init category entity.

---
+++ src/main/kotlin/com/learn/automated/testing/demo/features/category/Category.kt
@@ -0,0 +1,27 @@
+package com.learn.automated.testing.demo.features.category
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "category")
+@EntityListeners(AuditingEntityListener::class)
+class Category (
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private var id: Long?,
+
+        @Column(name = "name")
+        private var name: String,
+
+        @Column(name = "created_at")
+        @CreatedDate
+        private var createdAt: Date? = null,
+
+        @Column(name = "updated_at")
+        @LastModifiedDate
+        private var updatedAt: Date? = null
+)
